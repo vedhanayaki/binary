1cdb11a178a352d58a9ed1901e3a19af1bf0bfa5
vedhanayaki <vedhanayaki1996@gmail.com>
2017-07-16 23:06:08 -0700

    Add files via upload

---
+++ Binaryrepre.java
@@ -0,0 +1,14 @@
+package player;
+import java.util.Scanner;
+public class Binaryrepre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Enter the values:");
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+//Integer.toBinaryString(a);
+System.out.println("Binary Number is:"+Integer.toBinaryString(a));
+	}
+
+}
